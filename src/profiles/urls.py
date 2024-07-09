be0c0b9a1c87e8527b5acfbeb5b62f8fd73c1d8b
Ayubsoft254 <henryoayub15@gmail.com>
2024-07-09 08:48:57 +0300

    "Updated profiles/urls.py and profiles/views.py to separate profile view into list and detail views"

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from profiles.views import profile_view
+from profiles.views import profile_detail_view,profile_list_view
 
 urlpatterns = [
-    path("<str:username>/", profile_view, name="profiler"),    
+    path("<str:username>/", profile_detail_view, name="profile_detail_view"),
+    path("", profile_list_view, name="profiler_list_view"),    
 ]
