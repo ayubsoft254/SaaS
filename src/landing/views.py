698a3cc91b7205abae60f744662ebb1f35825a93
Ayubsoft254 <henryoayub15@gmail.com>
2024-07-11 11:10:07 +0300

    chore: Update landing page title
    This commit updates the landing page title to "Study sync" in the `main.html` template. This change reflects the rebranding of the SaaS product.

--- src/landing/views.py
+++ src/landing/views.py
@@ -8,9 +8,6 @@ from visits.models import visits_pagevisit
 
 def landing_dashboard_page_view(request):
     if request.user.is_authenticated:
-        return dashboard_view(request)
-    qs = visits_pagevisit.objects.all()
-    visits_pagevisit.objects.create(path=request.path)
-    page_views_formatted = helpers.numbers.shorten_number(qs.count() * 100_000)
-    social_views_formatted = helpers.numbers.shorten_number(qs.count() * 23_000)
-    return render(request, "landing/main.html", {"page_view_count": page_views_formatted, "social_views_count": social_views_formatted})
+        return dashboard_view(request)  
+    
+    return render(request, "landing/main.html", {})
